3da8fc0df83810632d87395fed63744eee3ceb21
Haitham Assoli <74528836+haithamassoli@users.noreply.github.com>
2021-10-07 15:53:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function getTime() {
+  const time = new Date();
+  const seconds = time.getSeconds();
+  const secondHand = document.querySelector(".second-hand");
+  const secondDeg = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDeg}deg)`;
+
+  const minHhand = document.querySelector(".min-hand");
+  const minutes = time.getMinutes();
+  const minutesDeg = (minutes / 60) * 360 + 90;
+  minHhand.style.transform = `rotate(${minutesDeg}deg)`;
+
+  const hours = time.getHours();
+  const hourHand = document.querySelector(".hour-hand");
+  const hourDeg = (hours / 60) * 360 + 90;
+
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+}
+
+setInterval(getTime, 1000);
